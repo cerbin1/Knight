46a808769232834b17d49a57eab2f4a8b0822272
Bartek <bartek9393@poczta.onet.pl>
2017-05-12 12:47:17 +0100

    Refactor

--- src/ChessKnight.java
+++ src/ChessKnight.java
@@ -1,27 +1,25 @@
-import static java.lang.Character.getNumericValue;
-
 public class ChessKnight {
     private static String possiblePositions;
 
     public static String getKnightMoves(String position) {
-        char column = position.charAt(0);
-        int row = getNumericValue(position.charAt(1));
+        char column = position.toLowerCase().charAt(0);
+        int row = position.charAt(1) - '0';
         possiblePositions = "";
 
-       checkPosition((char) (column - 2), row - 1);
-       checkPosition((char) (column - 2), row + 1);
-       checkPosition((char) (column - 1), row - 2);
-       checkPosition((char) (column - 1), row + 2);
-       checkPosition((char) (column + 1), row - 2);
-       checkPosition((char) (column + 1), row + 2);
-       checkPosition((char) (column + 2), row - 1);
-       checkPosition((char) (column + 2), row + 1);
+        checkPosition((char) (column - 2), row - 1);
+        checkPosition((char) (column - 2), row + 1);
+        checkPosition((char) (column - 1), row - 2);
+        checkPosition((char) (column - 1), row + 2);
+        checkPosition((char) (column + 1), row - 2);
+        checkPosition((char) (column + 1), row + 2);
+        checkPosition((char) (column + 2), row - 1);
+        checkPosition((char) (column + 2), row + 1);
 
-        return possiblePositions.toLowerCase();
+        return possiblePositions;
     }
 
     private static void checkPosition(char column, int row) {
-        if (1 <= row && row <= 8 && 'A' <= column && column <= 'H' || 'a' <= column && column <= 'h') {
+        if (1 <= row && row <= 8 && 'a' <= column && column <= 'h') {
             possiblePositions += column + Integer.toString(row) + " ";
         }
     }
